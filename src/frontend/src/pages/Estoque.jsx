d086fd86d9c92b26f892c91beba2ef310440c449
Kethlen Martins <kethlen.martins21@gmail.com>
2025-04-01 17:33:28 -0300

    feat: create new medicine component

--- src/frontend/src/pages/Estoque.jsx
+++ src/frontend/src/pages/Estoque.jsx
@@ -3,16 +3,24 @@ import SideBar from './components/Sidebar';
 import RemedioEstoque from './components/RemedioEstoque';
 import SearchBar from './components/SearchBar';
 import styles from '../styles/Estoque.module.css'
+import RemedioVencimento from './components/RemedioVencimento';
 
 export default function Estoque() {
     return (
         <div className="screen">
             <SideBar />
             <div className="content">
+                <h1 className={styles.title}>Estoque</h1>
+                <SearchBar />
                 <div className={styles.containerGeral}>
-                    <h1 className={styles.title}>Estoque</h1>
-                    <SearchBar />
+                <h2>Medicamentos próximos de acabar</h2>
                     <RemedioEstoque />
+                    <RemedioEstoque />
+                    <RemedioEstoque />
+                <h2>Medicamentos próximos do vencimento</h2>
+                    <RemedioVencimento />
+                    <RemedioVencimento />
+                    <RemedioVencimento />
                 </div>
             </div>
         </div>
