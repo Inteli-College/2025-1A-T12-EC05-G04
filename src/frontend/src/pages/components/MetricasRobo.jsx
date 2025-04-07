4bfcced8586b982fb977e4d66c56ad2b18c129cf
Kethlen Martins <kethlen.martins21@gmail.com>
2025-04-07 17:43:37 -0300

    feat: update cards title

--- src/frontend/src/pages/components/MetricasRobo.jsx
+++ src/frontend/src/pages/components/MetricasRobo.jsx
@@ -4,16 +4,16 @@ export default function MetricasRobo () {
             <h1>Manipulador robótico</h1>
             <div className="metricas-cards-container">
                 <div className="metricas-card">
-                    <h2>Taxa de Ocupação</h2>
-                    <p>75%</p>
+                    <h2>Horas trabalhadas</h2>
+                    <p>14h</p>
                 </div>
                 <div className="metricas-card">
-                    <h2>Tempo Médio de Espera</h2>
-                    <p>5 minutos</p>
+                    <h2>Taxa de erros</h2>
+                    <p>3%</p>
                 </div>
                 <div className="metricas-card">
-                    <h2>Taxa de Erros</h2>
-                    <p>1%</p>
+                    <h2>Tempo médio de montagem</h2>
+                    <p>2min30s</p>
                 </div>
                 <div className="metricas-card">
                     <h2>Tempo Médio de Resposta</h2>
